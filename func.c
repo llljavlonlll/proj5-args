4c921fdfad0dbe30a6f76b8db60b2a18492c9a5a
Butabaev <jbrxk@delmar.umsl.edu>
2017-11-17 22:01:50 -0600

    added substring checking code

--- func.c
+++ func.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 // Check if any string is a substring 
 // of other strings int the array
-void func()
+void func(char *strs[], int length)
 {
-
+	int i, j;
+	for(j = 0; j < length; j++) {
+		for(i = 0; i < length; i++) {
+			if(strstr(strs[i],strs[j]) != NULL) {
+				printf("%s is a substring of %s\n", strs[j], strs[i]);
+			}
+		}
+	}
 }
