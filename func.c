16cf9f90bd50ecdab9c18cf6742ae4f7f1471005
Butabaev <jbrxk@delmar.umsl.edu>
2017-11-17 22:36:57 -0600

    fixed a bug cause substring function to compare a string to itself

--- func.c
+++ func.c
@@ -8,7 +8,7 @@ void func(char *strs[], int length)
 	int i, j;
 	for(j = 0; j < length; j++) {
 		for(i = 0; i < length; i++) {
-			if(strstr(strs[i],strs[j]) != NULL) {
+			if(strstr(strs[i],strs[j]) != NULL && strs[j] != strs[i]) {
 				printf("%s is a substring of %s\n", strs[j], strs[i]);
 			}
 		}
