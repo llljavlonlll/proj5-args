072ddcf89ab99ec8cef76fb584d5e53d9cc84ab9
Butabaev <jbrxk@delmar.umsl.edu>
2017-11-17 22:40:09 -0600

    removed unnecessary outputs added space for output clearance

--- main.c
+++ main.c
@@ -23,33 +23,25 @@ int main(int argc, char *argv[])
 	for(i = 0; i < argc; i++) {
 		
 		if(isalpha(*argv[i])) {
-			printf("%s\n", argv[i]);
 			strs[str_count] = argv[i];
 			str_count++;
 		}
 		else if(isdigit(*argv[i])) {
-			printf("%s\n", argv[i]);
 			nums[num_count] = atoi(argv[i]);
 			num_count++;
 		}
 	}
 	
 	
-	printf("\nArray of strs: ");
-	for(i = 0; i < str_count; i++) {
-		printf("%s ", strs[i]);
-	}
-	
 	// Sum up numbers array
-	printf("\nArray of nums: ");
 	for(i = 0; i < num_count; i++) {
-		printf("%i ", nums[i]);
 		total += nums[i];
 	}
 
 	// Final output
-	printf("\n\nTotal integers: %i\n\n", total);
+	printf("\n\nTotal integers: %i\n", total);
 	func(strs, str_count);
-	
+
+	printf("\n\n");	
 	return 0;
 }
