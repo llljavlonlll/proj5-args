a2886c188513f459fb2c4a9dcee97d33fb36db50
Butabaev <jbrxk@delmar.umsl.edu>
2017-11-17 21:47:43 -0600

    added argument type detection code added code to store arguments in their respective arrays

--- func.c
+++ func.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+// Check if any string is a substring 
+// of other strings int the array
 void func()
 {
-	
+
 }
--- main.c
+++ main.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include "func.h"
+#include <ctype.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	int i, str_count = 0, num_count = 0, total;
+	int *nums;
+	char **strs;
+
+	nums = (int*) malloc((argc - 1) * sizeof(int));
+	strs = malloc((argc - 1) * sizeof(char*));
 	
+
+	if(argc == 1) {
+		printf("ERROR: I expect at least one argument!\n");
+		return 0;
+	}
+
+	// Determine type of the argument
+	// and store in respective arrays
+	for(i = 0; i < argc; i++) {
+		
+		if(isalpha(*argv[i])) {
+			printf("%s\n", argv[i]);
+			strs[str_count] = argv[i];
+			str_count++;
+		}
+		else if(isdigit(*argv[i])) {
+			printf("%s\n", argv[i]);
+			nums[num_count] = atoi(argv[i]);
+			num_count++;
+		}
+	}
+	
+	
+	printf("\nArray of strs: ");
+	for(i = 0; i < str_count; i++) {
+		printf("%s ", strs[i]);
+	}
+	
+	// Sum up numbers array
+	printf("\nArray of nums: ");
+	for(i = 0; i < num_count; i++) {
+		printf("%i ", nums[i]);
+		total += nums[i];
+	}
+
+	// Final output
+	printf("Total integers: %i\n\n", total);
+	
+	return 0;
 }
